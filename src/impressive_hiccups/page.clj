48c381ec6f51ce8be4e855d1cafee7136d232872
Gergely Nagy <algernon@madhouse-project.org>
2013-02-17 11:56:18 +0100

    page: Add support for options
    Instead of taking only a title option, turn the first argument into a
    map, and support other options like the URIs to the CSS, impress.js
    and of course the page title.
    
    If the first argument is not a map, then impress will call itself with
    a dummy options map, so that the defaults get used.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/impressive_hiccups/page.clj
+++ src/impressive_hiccups/page.clj
@@ -1,36 +1,43 @@
 (ns impressive-hiccups.page
   (:use [hiccup.element]
-        [hiccup.core]))
+        [hiccup.core]
+        [hiccup.util]
+        [hiccup.page]))
 
 (defmacro impress
-  [title & contents]
+  [options & contents]
 
-  `(html {:mode :html}
-         (doctype :html5)
-         [:html
-          [:head
-           [:meta {:charset "utf-8"
-                   :viewport "width=1024"}]
-           [:title ~title]
-           [:link {:href "css/main.css"
-                   :rel "stylesheet"}]
-           [:link {:href "http://fonts.googleapis.com/css?family=Josefin+Sans"
-                   :rel "stylesheet"}]]
+  (if-not (map? options)
+    `(impress {} ~options ~@contents)
+    `(let [options# ~options]
+       (html {:mode :html}
+             (doctype :html5)
+             [:html
+              [:head
+               [:meta {:charset "utf-8"
+                       :viewport "width=1024"}]
+               [:title (:title options#)]
+               [:link {:href (to-uri (or (:css-uri options#)
+                                         "css/main.css"))
+                       :rel "stylesheet"}]
+               [:link {:href (to-uri (or (:font-uri options#)
+                                         "http://fonts.googleapis.com/css?family=Josefin+Sans"))
+                       :rel "stylesheet"}]]
 
-          [:body
-           [:div {:class "fallback-message"}
-            [:p "Your browser "
-             [:strong "doesn't support the features required"]
-             " by impress.js, so you are preseqnted with a "
-             "simplified version of this presentation."]
+              [:body
+               [:div {:class "fallback-message"}
+                [:p "Your browser "
+                 [:strong "doesn't support the features required"]
+                 " by impress.js, so you are preseqnted with a "
+                 "simplified version of this presentation."]
 
-            [:p "For the best experience please use the latest"
-             [:strong "Chrome"] ", "
-             [:strong "Safari"] " or "
-             [:strong "Firefox"] " browser."]]
+                [:p "For the best experience please use the latest"
+                 [:strong "Chrome"] ", "
+                 [:strong "Safari"] " or "
+                 [:strong "Firefox"] " browser."]]
 
-           [:div {:id "impress"}
-            ~@contents]
-            
-           (include-js "js/impress.js")
-           (javascript-tag "(function(window, undefined){ var _onload = window.onload;  window.onload = function() { if(_onload){ _onload(); } impress().init(); }; }(this));")]]))
+               [:div {:id "impress"}
+                ~@contents]
+
+               (include-js (or (:impress-uri options#) "js/impress.js"))
+               (javascript-tag "(function(window, undefined){ var _onload = window.onload;  window.onload = function() { if(_onload){ _onload(); } impress().init(); }; }(this));")]]))))
